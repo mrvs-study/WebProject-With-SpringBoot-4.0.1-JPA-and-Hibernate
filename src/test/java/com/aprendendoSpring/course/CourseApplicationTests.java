71acf9321e0469aa1ff2a94918c404d6e65c3735
Marcos <marcosvinici667@gmail.com>
2026-01-15 00:50:33 -0300

    H2 database, test profile, JPA

--- src/test/java/com/aprendendoSpring/course/CourseApplicationTests.java
+++ src/test/java/com/aprendendoSpring/course/CourseApplicationTests.java
@@ -1,9 +1,8 @@
 package com.aprendendoSpring.course;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CourseApplicationTests {
 
 	@Test
