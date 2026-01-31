f365ea5e7efbe55bfe87f5b866c9e44f39159c1f
Marcos <marcosvinici667@gmail.com>
2026-01-30 23:03:49 -0300

    OrderStatus enum and created Category

--- src/main/java/com/aprendendoSpring/course/entities/Order.java
+++ src/main/java/com/aprendendoSpring/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.aprendendoSpring.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -31,16 +32,19 @@ public class Order implements Serializable {
 	@JoinColumn(name="client_id")
 	private User client;
 	
+	private Integer  orderStatus;
+	
 	
 	public Order() {
 		
 	}
 
-	public Order(Long id, Instant moment,User client) {
+	public Order(Long id, Instant moment,OrderStatus  orderStatus,User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 
@@ -67,6 +71,18 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+
+	public OrderStatus getOrderStatus() {
+		return  OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null){
+			this.orderStatus = orderStatus.getCode();
+		}
+			
 	}
 
 	@Override
